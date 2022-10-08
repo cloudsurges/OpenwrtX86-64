bf3588a81a5b14db629bf74df02b9fed42a24c26
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2022-10-08 12:08:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git leanpackages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git leanrouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
