5f4cefcccd83d9e8704493df1576ef136726502e
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-09-23 10:04:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/10.10.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.0.0.0/g' package/base-files/files/bin/config_generate
