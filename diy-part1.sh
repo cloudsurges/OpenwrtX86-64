37247cd2221e035cb73f3bced0a810134766e9d3
cloudsurge <96812044+cloudsurges@users.noreply.github.com>
2023-09-24 10:21:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-#echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
-#echo 'src-git adbyby-plus https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git koolproxyR https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-#echo 'src-git leanrouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+##echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+##echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+##echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+##echo 'src-git adbyby-plus https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+#echo 'src-git koolproxyR https://github.com/kenzok8/small-package' >>feeds.conf.default
+##echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+##echo 'src-git leanrouting https://github.com/coolsnowwolf/routing' >>feeds.conf.default
